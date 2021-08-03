8ee5d573ae88a2b37eb28344801522233e926c8b
pparkcoder <84856055+pparkcoder@users.noreply.github.com>
2021-08-03 15:51:09 +0900

    update code file

--- CODE/lyric_crawling.py
+++ CODE/lyric_crawling.py
@@ -3,6 +3,7 @@ import csv
 import urllib.parse
 import time
 import re
+import openpyxl
 import chromedriver_autoinstaller
 import subprocess
 from selenium.webdriver.chrome.options import Options
@@ -10,11 +11,10 @@ from selenium import webdriver
 
 # 0:곡제목, 1:가수, 2:장르, 3:발매일, 4:앨범명
 lyric_data = []
-with open('sample2.csv','r',encoding='utf-8-sig') as f:
+with open('sample.csv','r',encoding='utf-8-sig') as f:
     data = csv.reader(f)
-
+    num = 1
     for i in data:
-      
         ##header setting##
         try:
             subprocess.Popen(
@@ -36,10 +36,11 @@ with open('sample2.csv','r',encoding='utf-8-sig') as f:
         driver.implicitly_wait(10)
 
         ## 노래 찾기 ##
-        search = i[0] + " " + i[1] + " " + i[4]
+        search = i[0] + " " + i[1]
         url = 'https://vibe.naver.com/search/tracks?query='+urllib.parse.quote_plus(search)
         driver.get(url)
         time.sleep(0.5)
+        print(str(num)+'. '+search + ' 찾는 중..')
 
         ## 팝업창 닫기 ##
         # try :
@@ -52,16 +53,19 @@ with open('sample2.csv','r',encoding='utf-8-sig') as f:
         artist = driver.find_elements_by_css_selector('.artist')
         album = driver.find_elements_by_css_selector('.album')
         flag = 0
-        print(search + ' 찾는 중..')
 
-        ## 비교 ##
+        ## 1. 곡명, 가수명, 앨범명이 일치한 경우 / 2. 곡명, 가수명이 일치한 경우 순서로 찾음##
         if len(artist) > 0 :
             for j in range(1,len(artist)):
                 temp_song = song[j].text
                 temp_artist = artist[j].text
                 temp_album = re.sub('[\{\}\[\]\/?.`!^\-_+<>@\#$%&\\\=\'\"\♥\♡\ㅋ\ㅠ\ㅜ\ㄱ\ㅎ\ㄲ\ㅡ]', '', album[j].text)
-                if 'Inst' in temp_song:
+
+                ## Inst, MR version 은 건너뛰기 ##
+                if ('inst' in str(temp_song).lower()) or ('mr' in str(temp_song).lower()):
                     continue
+
+                ## 곡명, 가수명, 앨범명이 모두 일치한 경우 ##
                 if (i[0].lower() in str(temp_song).lower()) and (i[1] in temp_artist) and (re.sub('[\{\}\[\]\/?.`!^\-_+<>@\#$%&\\\=\'\"\♥\♡\ㅋ\ㅠ\ㅜ\ㄱ\ㅎ\ㄲ\ㅡ]', '',i[4].lower()) in str(temp_album).lower()):
                     flag = 1  # 동일 곡이 있는 경우
                     driver.find_element_by_xpath('//*[@id="content"]/div/div[3]/div/div/table/tbody/tr[' + str(j) + ']/td[3]/div[1]/span/a').click()
@@ -72,11 +76,35 @@ with open('sample2.csv','r',encoding='utf-8-sig') as f:
                     except: # 가사가 없는 경우
                         lyric_data.append('가사 없음')
                     break
+
+            if flag == 0 :
+                for j in range(1,len(artist)):
+                    temp_song = song[j].text
+                    temp_artist = artist[j].text
+
+                    ## Inst, MR version 은 건너뛰기 ##
+                    if ('inst' in str(temp_song).lower()) or ('mr' in str(temp_song).lower()):
+                        continue
+
+                    ## 곡명, 가수명이 모두 일치한 경우 ##
+                    if (i[0].lower() in str(temp_song).lower()) and (i[1] in temp_artist):
+                        flag = 1  # 동일 곡이 있는 경우
+                        driver.find_element_by_xpath('//*[@id="content"]/div/div[3]/div/div/table/tbody/tr[' + str(j) + ']/td[3]/div[1]/span/a').click()
+                        time.sleep(1)
+                        try: # 가사가 있는 경우
+                            lyric = driver.find_element_by_css_selector('.lyrics.hide')
+                            lyric_data.append(lyric.text)
+                        except: # 가사가 없는 경우
+                            lyric_data.append('가사 없음')
+                        break
         if flag == 0 :
-            lyric_data.append('동일 곡 없음')
+            lyric_data.append('곡 없음')
+        num += 1
         driver.close()
 
-with open('sample2가사.csv', 'w', encoding='utf-8-sig', newline='') as f:
-    csvWriter = csv.writer(f)
-    for i in lyric_data:
-      csvWriter.writerow(i)
+## 엑셀 저장 ##
+wb = openpyxl.Workbook()
+sheet = wb.active
+for i in range(len(lyric_data)):
+    sheet.cell(row = i+1,column=1).value = lyric_data[i]
+wb.save('sample가사.csv')
