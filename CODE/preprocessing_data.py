f85eb7cc9f2d9d851bc156394997517709cd05f6
pparkcoder <84856055+pparkcoder@users.noreply.github.com>
2021-08-20 17:20:43 +0900

    update code file

--- CODE/preprocessing_data.py
+++ CODE/preprocessing_data.py
@@ -11,6 +11,29 @@ with open('test.csv','r',encoding='utf-8-sig') as f:
             continue
         for j in range(0,len(i)):
             temp.append(i[j])
+            
+        date = i[3]
+        year = date[:4]
+        month = date[4:6]
+        if year < '1990':
+            temp_data = '1990년대 이전'
+        elif '1990' <= year and year < '2000':
+            temp_data = '1990년대'
+        elif '2000' <= year and year < '2010':
+            temp_data = '2000년대'
+        elif '2010' <= year:
+            temp_data = '2010년 이후'
+        temp.append(temp_data)
+
+        if '03' <= month and month <= '05':
+            temp_data = '봄'
+        elif '06' <= month and month <= '08':
+            temp_data = '여름'
+        elif '09' <= month and month <= '11':
+            temp_data = '가을'
+        else:
+            temp_data = '겨울'
+            
         result.append(temp)
 
 with open('test_가사.csv', 'w', encoding='utf-8-sig', newline='') as f:
