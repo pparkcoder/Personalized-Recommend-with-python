15d67c16e44beeb3eaf018007ed417eab19de066
pparkcoder <pparkcoder@gmail.com>
2021-09-02 16:55:20 +0900

    README 작성

--- CODE/basic_api.py
+++ CODE/basic_api.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
+resource = [] #서버 리소스
 @app.route('/')
 def hello():
     return 'Hello Flask'
@@ -9,9 +10,13 @@ def hello():
 def get_name(name):
     return "hello {}".format(name)
 
-@app.route('/user/<int:id>') # id는 정수형 타입
-def get_user(id):
-    return "user id is {}".format(id)
+@app.route('/user/<int:user_id>', methods = ['GET']) # id는 정수형 타입
+def get_user(user_id):
+    for user in resource :
+        if user['user_id'] is user_id :
+            return jsonify(user)
+
+    return jsonify(None)
 
 @app.route('/json/<int:dest_id>/<message>')
 @app.route('/JSON/<int:dest_id>/<message>')
